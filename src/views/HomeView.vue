686bb2394b6b55c43921d0020356b5dc793a171e
Henrik Engqvist <henrik@engqvist.org>
2024-02-19 15:28:10 +0100

    Project cleanup (#3)
    * refactor: removed styling.
    
    * Removed logo
    
    * refactor: removed unused components.
    
    * refactor: removed AboutView component & routing.
    
    * refactor: removed imports from HomeView component.
    
    * refactor: removed unused imports & styling from App.Vue.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home View</h1>
   </main>
 </template>
+
+<style scoped>
+</style>
