c3ece96d45c6082c1ea1e3e76aeda8217cfefcc8
William Mattsson <wmattsson@hotmail.com>
2024-02-21 12:25:24 +0100

    Added placeholder logo

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+<nav>
+    <router-link to="/">
+        <img src="../assets/CoinCompassLogo.png" alt="logo">
+    </router-link>
+</nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+import NavBar from '../components/NavBar.vue'
 </script>
 
 <template>
   <main>
+    <NavBar />
     <h1>Home View</h1>
   </main>
 </template>
