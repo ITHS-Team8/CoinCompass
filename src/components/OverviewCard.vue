9c1cbe7272b4ce39b46a3cc312d3f5e3039fafa0
hydrarguru <henrik@engqvist.org>
2024-02-21 12:35:45 +0100

    Added OverviewCard component styling

--- src/components/OverviewCard.vue
+++ src/components/OverviewCard.vue
@@ -6,10 +6,47 @@ defineProps({
 </script>
 
 <template>
-    <div>
-
+    <div class="main-container">
+        <div class="title">
+            <h1>{{ cardTitle }}</h1>
+        </div>
+        <div class="content-container">
+            <p>{{ cardInfo }}</p>
+        </div>
     </div>
 </template>
 
 <style scoped>
+    .main-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        border-radius: 12px;
+
+        background-color: #D9D9D9;
+    }
+    .main-container h1 {
+        font-style: italic;
+        font-weight: normal;
+        font-size: 2rem;
+        margin: 0.5rem;
+    }
+    .title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #C6C4C4;
+        border-radius: 12px 12px 0 0;
+    }
+    .content-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 125px;
+    }
+    .content-container p {
+        font-size: 2rem;
+    }
 </style>
