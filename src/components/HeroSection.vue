478bae2b372e5adc22b7440aa94a62e3bb516803
William Mattsson <wmattsson@hotmail.com>
2024-02-22 13:39:23 +0100

    Add HeroSection component and hero-img1

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,11 @@
+<template>
+    <section id="hero-section">
+        <div class="hero-img-container">
+            <img src="../assets/hero1.png" alt="hero img of globe" />
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
     import NavBar from '../components/NavBar.vue'
+    import HeroSection from '../components/HeroSection.vue'
 </script>
 
 <template>
     <main>
         <NavBar view="Home" />
+        <HeroSection />
         <h1>Home View</h1>
     </main>
 </template>
