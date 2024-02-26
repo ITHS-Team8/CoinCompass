7eed46be9b34584e453875629487401325113705
Freste03 <94680249+Freste03@users.noreply.github.com>
2024-02-26 10:54:34 +0100

    Footer (#24)
    * Completed footer
    
    * Added lang ts to footer component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
     import NavBar from '../components/NavBar.vue'
     import HeroSection from '../components/HeroSection.vue'
+    import FooterComponent from '@/components/FooterComponent.vue';
 </script>
 
 <template>
     <main>
         <NavBar />
         <HeroSection />
+        <FooterComponent />
     </main>
 </template>
 
