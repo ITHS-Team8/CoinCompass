65f831fcedeabca4a4f2882a22bccb741f594758
hydrarguru <henrik@engqvist.org>
2024-02-26 21:03:53 +0100

    Add ProfileAvatar component.

---
+++ src/components/ProfileAvatar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+    userName: {
+        type: String,
+        required: true,
+        default: 'userName'
+    },
+    avatarUrl: String,
+})
+</script>
+
+<template>
+    <section>
+        <p>{{ userName }}</p>
+        <img :src="avatarUrl" alt="avatar" />
+        <button>Change Avatar</button>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
