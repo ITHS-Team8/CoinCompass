8ef7249d96da837daeb82d9413adeb3bac3f7285
hydrarguru <henrik@engqvist.org>
2024-02-26 21:23:27 +0100

    Added ProfileAvatar component styling

--- src/components/ProfileAvatar.vue
+++ src/components/ProfileAvatar.vue
@@ -12,13 +12,46 @@ defineProps({
 </script>
 
 <template>
-    <section>
-        <p>{{ userName }}</p>
-        <img :src="avatarUrl" alt="avatar" />
-        <button>Change Avatar</button>
+    <section class="avatar-container">
+        <h1 class="username">{{ userName }}</h1>
+        <img class="avatar-image" :src="avatarUrl" alt="avatar" />
+        <button class="avatar-button">Change Avatar</button>
     </section>
 </template>
 
 <style scoped>
+    .avatar-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
+    .avatar-image {
+        width: 25vh;
+        height: 25vh;
+        border-radius: 50%;
+        margin: 1rem;
+    }
+
+    .username {
+        margin: .75rem;
+        font-size: 1.5rem;
+        font-style: italic;
+    }
+
+    .avatar-button {
+        margin: 1rem;
+        padding: 0.75rem 1rem;
+        background-color: var(--primary-color);
+        color: #FFFF;
+        border-radius: 6px;
+        border: none;
+        font-size: 0.75rem;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    }
+
+    .avatar-button:hover {
+        background-color: var(--secondary-color);
+        transition: all 0.3s ease-in-out;
+    }
 </style>
