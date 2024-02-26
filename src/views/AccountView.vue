49a9ddef18a48f5bfa444a9987746037524b8d2c
Henrik Engqvist <henrik@engqvist.org>
2024-02-26 21:28:08 +0100

    Avatar component (#25)
    * Add ProfileAvatar component.
    
    * Refactor ProfileAvatar component props
    
    * Added ProfileAvatar component styling
    
    * Add profile avatar component to AccountView.vue

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
     import NavBar from '../components/NavBar.vue'
+    import ProfileAvatar from '../components/ProfileAvatar.vue'
 </script>
 
 <template>
+    <NavBar />
     <main>
-        <NavBar />
-        <h1>Account View</h1>
+        <ProfileAvatar userName="userName" avatarUrl="https://placehold.co/250" />
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+    main {
+        margin-top: 10vh;
+    }
+</style>
