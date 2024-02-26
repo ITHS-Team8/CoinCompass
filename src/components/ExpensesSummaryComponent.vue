47468c7b99ee5c392ca65f3cd0935ec3be25ecbf
William Mattsson <wmattsson@hotmail.com>
2024-02-26 10:39:16 +0100

    Added html and simple styling

--- src/components/ExpensesSummaryComponent.vue
+++ src/components/ExpensesSummaryComponent.vue
@@ -1,7 +1,35 @@
 <template>
     <h1>Expenses summary</h1>
+    <div class="container">
+        <div class="text-container">
+            <span class="total-text">Total expenses</span>
+            <span class="total-text">$500</span>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+    .container {
+        margin: auto;
+        margin-top: 10vh; /* Temporary för att se den */
+
+        display: flex;
+        justify-content: center;
+    }
+    .text-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #eaeaea;
+        padding: 1.2rem;
+        width: 90%;
+        border-radius: 12px;
+    }
+
+    .total-text {
+        font-weight: 600;
+        font-size: 1.2rem;
+    }
+</style>
