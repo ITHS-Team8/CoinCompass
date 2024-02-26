a4d93fcc56de8116531010d7f07e2fdc5ee73170
William Mattsson <wmattsson@hotmail.com>
2024-02-26 10:18:24 +0100

    Added expenses summary component and imported it

--- src/views/ExpensesView.vue
+++ src/views/ExpensesView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
     import NavBar from '../components/NavBar.vue'
+    import ExpensesSummary from '../components/ExpensesSummaryComponent.vue'
 </script>
 
 <template>
     <main>
         <NavBar />
+        <ExpensesSummary />
         <h1>Expenses View</h1>
     </main>
 </template>
