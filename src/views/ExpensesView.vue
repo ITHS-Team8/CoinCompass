8a058280dce31b414ce74f0cf1a4a8ee1b4b02df
William Mattsson <wmattsson@hotmail.com>
2024-02-27 15:29:55 +0100

    Added latest purchase in card 3

--- src/views/ExpensesView.vue
+++ src/views/ExpensesView.vue
@@ -5,11 +5,15 @@
 
     import { useExpensesStore } from '../stores/ExpensesStore'
     const expensesStore = useExpensesStore()
+
+    const latestExpense =
+        expensesStore.expenses[expensesStore.expenses.length - 1]
 </script>
 
 <template>
     <NavBar />
     <main>
+        <h2 class="over-view-title">Overview</h2>
         <div class="overview-cards-container">
             <OverviewCard
                 card-title="Total expenses"
@@ -19,7 +23,10 @@
                 card-title="Number of transactions"
                 :card-info="`${expensesStore.expenses.length}st`"
             />
-            <OverviewCard card-title="'Hej'" card-info="asdfasdfasdf" />
+            <OverviewCard
+                card-title="Latest purchase"
+                :card-info="`${latestExpense.name} - ${latestExpense.amount}kr`"
+            />
             <OverviewCard card-title="'Hej'" card-info="asdfasdfasdf" />
         </div>
 
@@ -27,4 +34,21 @@
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+    main {
+        margin-top: 12vh;
+    }
+
+    .over-view-title {
+        text-align: center;
+        font-size: 2.4rem;
+        margin-top: 1rem;
+    }
+    .overview-cards-container {
+        display: flex;
+        justify-content: space-between;
+        margin: auto;
+        margin-top: 1rem;
+        width: 90%;
+    }
+</style>
