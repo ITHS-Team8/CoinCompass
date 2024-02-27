d8105740c57f50b842cd12060428ef55b65bf738
William Mattsson <wmattsson@hotmail.com>
2024-02-27 14:53:53 +0100

    Imported OverviewCard #5 and expensesStore

--- src/views/ExpensesView.vue
+++ src/views/ExpensesView.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
     import NavBar from '../components/NavBar.vue'
     import ExpensesSummary from '../components/ExpensesSummaryComponent.vue'
+    import OverviewCard from '../components/OverviewCard.vue'
+
+    import { useExpensesStore } from '../stores/ExpensesStore'
+    const expensesStore = useExpensesStore()
 </script>
 
 <template>
-  <NavBar />
+    <NavBar />
     <main>
         <ExpensesSummary />
     </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
