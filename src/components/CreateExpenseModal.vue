29fd15019d9f652c5224562d4cd9d9af66c80d66
hydrarguru <henrik@engqvist.org>
2024-02-28 21:25:27 +0100

    Added props & exposed methods.

--- src/components/CreateExpenseModal.vue
+++ src/components/CreateExpenseModal.vue
@@ -1,5 +1,26 @@
-<script lang="ts">
+<script setup lang="ts">
+    import { ref } from "vue";
+    const dialog = ref<HTMLDialogElement>();
 
+    const props = defineProps({
+      classes: {
+        type: String,
+        default: "",
+      },
+    })
+
+    const visible = ref(false);
+
+    const showModal = () => {
+      dialog.value?.showModal();
+      visible.value = true;
+    };
+
+    defineExpose({
+      show: showModal,
+      close: (returnVal?: string): void => dialog.value?.close(returnVal),
+      visible,
+    })
 </script>
 
 <template>
