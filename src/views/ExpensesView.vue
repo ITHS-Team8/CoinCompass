98dc000b279b09cca87eda80bd095be1c9f781c4
hydrarguru <henrik@engqvist.org>
2024-02-29 13:24:28 +0100

    Added modal to ExpenseView.

--- src/views/ExpensesView.vue
+++ src/views/ExpensesView.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
+    import { ref } from 'vue'
     import NavBar from '../components/NavBar.vue'
+    import CreateExpanseModal from '../components/CreateExpenseModal.vue'
+
+    const modal = ref<InstanceType<typeof CreateExpanseModal>>();
+    const showModal = () => modal.value?.show();
 </script>
 
 <template>
+    <NavBar />
     <main>
-        <NavBar />
+        <CreateExpanseModal ref="modal" showCancel />
+        <button @click="showModal">Create Expense</button>
     </main>
 </template>
 
 <style scoped>
+    main {
+        margin-top: 10vh;
+    }
 </style>
