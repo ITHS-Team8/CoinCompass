266a18c3b4134db9a6be63d67dc82e22643c101c
Henrik Engqvist <henrik@engqvist.org>
2024-02-29 13:28:37 +0100

    CreateExpenseModal (#29)
    * Add CreateExpenseModal component
    
    * Added props & exposed methods.
    
    * Add expense creation form.
    
    * Add expense description field.
    
    * Basic styling.
    
    * refactor: form structure
    
    * Refactor form container styles
    
    * Update styling for date input field
    
    * Removed props.
    
    * refactor: form styling.
    
    * Update textarea styling.
    
    * Refactor: removed blue outline on focus.
    
    * refactor: fixed dialog styling to place element in middle.
    
    * Added modal to ExpenseView.

--- src/views/ExpensesView.vue
+++ src/views/ExpensesView.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
+    import { ref } from 'vue'
     import NavBar from '../components/NavBar.vue'
     import ExpensesSummary from '../components/ExpensesSummaryComponent.vue'
+    
+    import CreateExpanseModal from '../components/CreateExpenseModal.vue'
+    const modal = ref<InstanceType<typeof CreateExpanseModal>>();
+    const showModal = () => modal.value?.show();
 </script>
 
 <template>
-  <NavBar />
+    <NavBar />
     <main>
+        <CreateExpanseModal ref="modal" showCancel />
+        <button @click="showModal">Create Expense</button>
         <ExpensesSummary />
-    </main>
 </template>
 
 <style scoped>
+    main {
+        margin-top: 10vh;
+    }
 </style>
