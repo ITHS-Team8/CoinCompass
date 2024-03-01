738289570fc9c88696d2b3e7ce303414c93f8281
Freste03 <freddiestenqvist@gmail.com>
2024-03-01 13:57:41 +0100

    Fully functional table

--- src/views/ExpensesView.vue
+++ src/views/ExpensesView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
     import NavBar from '../components/NavBar.vue'
     import ExpensesSummary from '../components/ExpensesSummaryComponent.vue'
+    import ExpensesTable from '../components/ExpensesTable.vue';
 </script>
 
 <template>
   <NavBar />
     <main>
+        <ExpensesTable />
         <ExpensesSummary />
     </main>
 </template>
