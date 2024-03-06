bcaab836f20950260e1a9cab74a2d373dc8130ec
William Mattsson <wmattsson@hotmail.com>
2024-03-06 18:56:47 +0100

    Created LoginComponent #34

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Login Component</h1>
+    <h1>Login Component</h1>
+    <h1>Login Component</h1>
+    <h1>Login Component</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,13 @@
 <template>
     <NavBar />
-    <h1>Login</h1>
-    <h1>Login</h1>
-    <h1>Login</h1>
-    <h1>Login</h1>
+    <LoginComponent />
     <Footer />
 </template>
 
 <script setup lang="ts">
     import NavBar from '@/components/NavBar.vue'
     import Footer from '@/components/FooterComponent.vue'
+    import LoginComponent from '@/components/LoginComponent.vue'
 </script>
 
 <style scoped></style>
