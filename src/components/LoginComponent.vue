dd04fb16a53d72e0c1b9ed018a4460c118c8b595
William Mattsson <wmattsson@hotmail.com>
2024-03-06 19:40:40 +0100

    Added login button to navbar

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,10 +1,11 @@
 <template>
     <h1>Login Component</h1>
-    <h1>Login Component</h1>
-    <h1>Login Component</h1>
-    <h1>Login Component</h1>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+    h1 {
+        margin-top: 10vh;
+    }
+</style>
