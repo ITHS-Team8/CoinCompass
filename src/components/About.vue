9dbb639a4b53451175dddd41aeaeeb5ba1edd743
Naodtess <144320067+Naodtess@users.noreply.github.com>
2024-03-07 13:16:59 +0100

    Added about vue

---
+++ src/components/About.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div class="about">
+    <div class="container mx-auto px-6 py-10">
+      <h1 class="text-4xl font-bold text-center mb-4">About CoinCompass</h1>
+      <p class="text-lg text-gray-700 text-center mb-8">
+        CoinCompass is your trusted partner in financial management. We've been helping users from all over the globe navigate the complexities of personal finance with our intuitive expense tracking platform.
+      </p>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div class="mission">
+          <h2 class="text-3xl font-semibold mb-3">Our Mission</h2>
+          <p class="text-gray-600">
+            We aim to empower individuals to make informed financial decisions. With real-time data tracking and insightful analytics, we make finance easy and accessible for everyone.
+          </p>
+        </div>
+
+        <div class="vision">
+          <h2 class="text-3xl font-semibold mb-3">Our Vision</h2>
+          <p class="text-gray-600">
+            Our vision is a world where financial literacy is not a privilege but a common standard. CoinCompass is dedicated to contributing to this future by offering tools that educate and enable users to take charge of their finances.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-    import NavBar from '../components/NavBar.vue'
-    import ProfileAvatar from '../components/ProfileAvatar.vue'
+import NavBar from '../components/NavBar.vue'
+import ProfileAvatar from '../components/ProfileAvatar.vue'
+import AccountSettingsVue from '../components/AccountSettings.vue';
 </script>
 
 <template>
     <NavBar />
     <main>
         <ProfileAvatar userName="userName" avatarUrl="https://placehold.co/250" />
+        <AccountSettingsVue />
     </main>
 </template>
 
 <style scoped>
-    main {
-        margin-top: 10vh;
-    }
+main {
+    margin-top: 10vh;
+}
 </style>
