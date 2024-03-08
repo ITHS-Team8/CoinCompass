eea652a162bf25698911ec7b4ce10386ecf34673
hydrarguru <henrik@engqvist.org>
2024-03-08 12:19:24 +0100

    feat: Update Expense functionality.

--- src/firebase/database.ts
+++ src/firebase/database.ts
@@ -21,9 +21,36 @@ export async function addExpense(name: string, description: string, amount: numb
             expenseDescription: description,
             expenseAmount: amount,
             expenseCategory: category,
-            createdAt: Date.now()
+            createdAt: Date.now(),
+            modifiedAt: Date.now()
         });
       } catch (e) {
         console.error("Error adding document: ", e);
       }
+}
+
+
+
+/**
+ * Updates an expense in the database.
+ * @param expenseId - The ID of the expense to update.
+ * @param updatedName - The updated name of the expense.
+ * @param updatedDescription - The updated description of the expense.
+ * @param updatedAmount - The updated amount of the expense.
+ * @param updatedCategory - The updated category of the expense.
+ * @returns A promise that resolves when the update is successful, or rejects with an error.
+ */
+export async function updateExpense(expenseId: string, updatedName: string, updatedDescription: string, updatedAmount: number, updatedCategory: string) {
+    try {
+        await setDoc(doc(db, "expenses", expenseId), {
+            expenseId: expenseId,
+            expenseName: updatedName,
+            expenseDescription: updatedDescription,
+            expenseAmount: updatedAmount,
+            expenseCategory: updatedCategory,
+            modifedAt: Date.now()
+        });
+      } catch (e) {
+        console.error("Error updating document: ", e);
+    }
 }
