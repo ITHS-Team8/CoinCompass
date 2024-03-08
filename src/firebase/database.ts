e46270b17e8b8fd609e400418cdc185ba27afc98
hydrarguru <henrik@engqvist.org>
2024-03-08 12:18:52 +0100

    doc: added documentation for addExpense.

--- src/firebase/database.ts
+++ src/firebase/database.ts
@@ -1,6 +1,17 @@
 import db from '@/firebase'
 import { doc, setDoc } from "firebase/firestore";
 
+/**
+ * Adds an expense to the database.
+ * 
+ * @param name - The name of the expense.
+ * @param description - The description of the expense.
+ * @param amount - The amount of the expense.
+ * @param category - The category of the expense.
+ * @returns A promise that resolves when the expense is successfully added to the database.
+ * @throws An error if there is an issue adding the document to the database.
+ * 
+ */
 export async function addExpense(name: string, description: string, amount: number, category: string) {
     try {
         const randomUUID = crypto.randomUUID();
