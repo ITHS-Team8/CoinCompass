7429a728ff6b055ef1092d946de4266d0a5b4633
hydrarguru <henrik@engqvist.org>
2024-03-08 09:51:45 +0100

    removed drizzle from project.

--- drizzle/0001_aberrant_magneto.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `projects` RENAME TO `expenses`;--> statement-breakpoint
-DROP INDEX IF EXISTS `projects_id_unique`;--> statement-breakpoint
-CREATE UNIQUE INDEX `expenses_id_unique` ON `expenses` (`id`);
