e8fdb39c2a1cd49cfa9062e57af01c44a68c2f2a
hydrarguru <henrik@engqvist.org>
2024-03-08 17:06:59 +0100

    Refactor ExpensesView.vue component to use data from firebase database.

--- src/views/ExpensesView.vue
+++ src/views/ExpensesView.vue
@@ -5,54 +5,57 @@
     import ExpensesSummary from '../components/ExpensesSummaryComponent.vue'
     import ExpensesTable from '../components/ExpensesTable.vue'
     import FooterComponent from '../components/FooterComponent.vue'
-
-    import EditExpenseModal from '../components/EditExpenseModal.vue'
-    const editModal = ref<InstanceType<typeof EditExpenseModal>>()
-    const showEditModal = () => editModal.value?.show()
-    
-    import CreateExpanseModal from '../components/CreateExpenseModal.vue'
-    const modal = ref<InstanceType<typeof CreateExpanseModal>>()
-    const showModal = () => modal.value?.show()
     import OverviewCard from '../components/OverviewCard.vue'
 
-    import { useExpensesStore } from '../stores/ExpensesStore'
-    const expensesStore = useExpensesStore()
+    import CreateExpense from '@/components/CreateExpense.vue'
+    import ModalComponent from '@/components/ModalComponent.vue'
+    const modal = ref<InstanceType<typeof ModalComponent>>()
+    const showModal = () => modal.value?.show()
+
+    import { getExpenses } from '@/firebase/database';
+    const expenses = await getExpenses();
+
     const latestExpense = computed(() => {
-        return expensesStore.expenses[expensesStore.expenses.length - 1]
+        return expenses[expenses.length - 1]
+    })
+    const totalAmount = computed(() => {
+        return expenses.reduce((acc, expense) => acc + expense.expenseAmount, 0)
     })
 </script>
 
 <template>
     <NavBar />
     <main>
-        <CreateExpanseModal ref="modal" showCancel />
+        <ModalComponent ref="modal">
+            <CreateExpense />
+        </ModalComponent>
         <h2 class="over-view-title">Overview</h2>
         <div class="overview-cards-container">
             <OverviewCard
                 class="overview-card"
                 card-title="Total expenses"
-                :card-info="`${expensesStore.totalAmount}kr`"
+                :card-info="`${totalAmount}kr`"
             />
             <OverviewCard
                 class="overview-card"
                 card-title="Total transactions"
-                :card-info="`${expensesStore.expenses.length}st`"
+                :card-info="`${expenses.length}st`"
             />
             <OverviewCard
-                v-if="expensesStore.expenses.length > 0"
+                v-if="expenses.length > 0"
                 class="overview-card"
                 card-title="Average cost"
-                :card-info="`${(expensesStore.totalAmount / expensesStore.expenses.length).toFixed(2)}kr`"
+                :card-info="`${(totalAmount / expenses.length).toFixed(2)}kr`"
             />
             <OverviewCard
                 v-if="latestExpense"
                 class="overview-card"
                 card-title="Latest purchase"
-                :card-info="`${latestExpense.name} - ${latestExpense.amount}kr`"
+                :card-info="`${latestExpense.expenseName} - ${latestExpense.expenseAmount}kr`"
             />
         </div>
 
-        <h3 class="table-heading" v-if="expensesStore.expenses.length > 0">
+        <h3 class="table-heading" v-if="expenses.length > 0">
             All Expenses
         </h3>
         <div class="no-expenses" v-else>
