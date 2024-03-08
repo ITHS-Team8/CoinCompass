5762bb2dac9396b1f11cc8dc145ebde57e6cd4a4
hydrarguru <henrik@engqvist.org>
2024-03-08 10:48:53 +0100

    Removed drizzle from project.

--- src/data/db.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
-import { users } from '../data/schema';
-
-const sqlite = new Database('src/data/database.sqlite3');
-const db = drizzle(sqlite);
-
-const result = await db.select().from(users);
