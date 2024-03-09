1201d9b4f6c84915585497524b28e4122d33d2c6
Naodtess <144320067+Naodtess@users.noreply.github.com>
2024-03-09 18:20:39 +0100

    AccountSettings komponent & AboutView. (#47)
    * Added about vue
    
    * fix
    
    * fix
    
    * refactor: moved script to top of file.
    
    * refactor: moved script tag to top of file + formatting.
    
    * removed unused vue component.
    
    * moved navbar component outside of <main>
    
    * style: <main> styling
    
    * refactor: AboutView.vue
    
    * refactor: added footer component to AboutView.
    
    * Refactor AccountView.vue to include PersonalInfo and update AccountSettings component
    
    ---------
    
    Co-authored-by: hydrarguru <henrik@engqvist.org>

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,57 @@
-<script setup lang="ts">
-    import NavBar from '../components/NavBar.vue'
+<script setup>
+import NavBar from "../components/NavBar.vue";
+import FooterComponent from '@/components/FooterComponent.vue';
 </script>
 
 <template>
-    <main>
-        <NavBar />
-        <h1>About View</h1>
-    </main>
+  <NavBar />
+  <main>
+    <div class="container">
+      <h1>Om Oss</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+  </main>
+  <FooterComponent />
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  margin-top: 12vh;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 800px;
+  margin: 0 auto;
+}
+.container p {
+  margin: 1rem;
+}
+</style>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
-    import NavBar from '../components/NavBar.vue'
-    import ProfileAvatar from '../components/ProfileAvatar.vue'
+import NavBar from '../components/NavBar.vue'
+import ProfileAvatar from '../components/ProfileAvatar.vue'
+import AccountSettings from '../components/AccountSettings.vue';
+import PersonalInfo from '@/components/PersonalInfo.vue';
 </script>
 
 <template>
     <NavBar />
     <main>
         <ProfileAvatar userName="userName" avatarUrl="https://placehold.co/250" />
+        <div class="top-container">
+            <PersonalInfo />
+            <div class="settings-container">
+                <h2>Inställningar</h2>
+                <AccountSettings />
+            </div>
+        </div>
     </main>
 </template>
 
 <style scoped>
-    main {
-        margin-top: 10vh;
-    }
+main {
+    margin-top: 10vh;
+}
+.top-container {
+    display: flex;
+    flex-direction: row;
+    margin: 0 10vw;
+    gap: 2rem;
+}
+.settings-container {
+    background-color: #9F9D9D;
+    padding: 20px;
+}
 </style>
