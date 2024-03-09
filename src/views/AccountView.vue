7052f94a6da7d77e95adbbd639c531de6d8bae7a
hydrarguru <henrik@engqvist.org>
2024-03-09 18:19:01 +0100

    Refactor AccountView.vue to include PersonalInfo and update AccountSettings component

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
 import NavBar from '../components/NavBar.vue'
 import ProfileAvatar from '../components/ProfileAvatar.vue'
-import AccountSettingsVue from '../components/AccountSettings.vue';
+import AccountSettings from '../components/AccountSettings.vue';
+import PersonalInfo from '@/components/PersonalInfo.vue';
 </script>
 
 <template>
     <NavBar />
     <main>
         <ProfileAvatar userName="userName" avatarUrl="https://placehold.co/250" />
-        <AccountSettingsVue />
+        <div class="top-container">
+            <PersonalInfo />
+            <div class="settings-container">
+                <h2>Inställningar</h2>
+                <AccountSettings />
+            </div>
+        </div>
     </main>
 </template>
 
 <style scoped>
 main {
     margin-top: 10vh;
+}
+.top-container {
+    display: flex;
+    flex-direction: row;
+    margin: 0 10vw;
+    gap: 2rem;
+}
+.settings-container {
+    background-color: #9F9D9D;
+    padding: 20px;
 }
 </style>
