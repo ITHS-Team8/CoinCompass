9ed7ce337d47a090c97c9eb16d47aa03f7a4e3b5
William Mattsson <wmattsson@hotmail.com>
2024-03-11 10:39:09 +0100

    Removed change avatar button

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,36 +1,39 @@
 <script setup lang="ts">
-import NavBar from '../components/NavBar.vue'
-import ProfileAvatar from '../components/ProfileAvatar.vue'
+import NavBar from '../components/NavBar.vue';
+import ProfileAvatar from '../components/ProfileAvatar.vue';
 import AccountSettings from '../components/AccountSettings.vue';
 import PersonalInfo from '@/components/PersonalInfo.vue';
 </script>
 
 <template>
-    <NavBar />
-    <main>
-        <ProfileAvatar userName="userName" avatarUrl="https://placehold.co/250" />
-        <div class="top-container">
-            <PersonalInfo />
-            <div class="settings-container">
-                <h2>Inställningar</h2>
-                <AccountSettings />
-            </div>
-        </div>
-    </main>
+	<NavBar />
+	<main>
+		<ProfileAvatar
+			userName="userName"
+			avatarUrl="https://placehold.co/250"
+		/>
+		<div class="top-container">
+			<PersonalInfo />
+			<div class="settings-container">
+				<h2>Inställningar</h2>
+				<AccountSettings />
+			</div>
+		</div>
+	</main>
 </template>
 
 <style scoped>
 main {
-    margin-top: 10vh;
+	margin-top: 12vh;
 }
 .top-container {
-    display: flex;
-    flex-direction: row;
-    margin: 0 10vw;
-    gap: 2rem;
+	display: flex;
+	flex-direction: row;
+	margin: 0 10vw;
+	gap: 2rem;
 }
 .settings-container {
-    background-color: #9F9D9D;
-    padding: 20px;
+	background-color: #9f9d9d;
+	padding: 20px;
 }
 </style>
