e3214a357fa0998371775204427c3f102af4c859
William Mattsson <143711975+WilliamMattsson1@users.noreply.github.com>
2024-03-11 17:04:27 +0100

    Added changes (#62)

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -1,58 +1,64 @@
 <script setup lang="ts">
-    import { getUserDetails} from '@/firebase/database';
-    const userDetails = await getUserDetails();
+import { getUserDetails } from '@/firebase/database';
+const userDetails = await getUserDetails();
 </script>
 
 <template>
-    <div class="container">
-        <div class="title-container">
-            <h1>Personal Information</h1>
-        </div>
-        <div class="top-container">
-                <h3>Username:</h3><p>{{ userDetails.username }}</p>
-                <h3>Email:</h3><p>{{ userDetails.email }}</p>
-                <h3>First Name:</h3><p>{{ userDetails.firstName }}</p>
-                <h3>Last Name:</h3><p>{{ userDetails.lastName }}</p>
-                <h3>Date of Birth:</h3><p>{{ userDetails.dateOfBirth }}</p>
-                <h3>Gender:</h3><p>{{ userDetails.gender }}</p>
-        </div>
-    </div>
+	<div class="container">
+		<div class="title-container">
+			<h1>Personal Information</h1>
+		</div>
+		<div class="top-container">
+			<h3>Username:</h3>
+			<p>{{ userDetails.username }}</p>
+			<h3>Email:</h3>
+			<p>{{ userDetails.email }}</p>
+			<h3>First Name:</h3>
+			<p>{{ userDetails.firstName }}</p>
+			<h3>Last Name:</h3>
+			<p>{{ userDetails.lastName }}</p>
+			<h3>Date of Birth:</h3>
+			<p>{{ userDetails.dateOfBirth }}</p>
+			<h3>Gender:</h3>
+			<p>{{ userDetails.gender }}</p>
+		</div>
+	</div>
 </template>
 
 <style scoped>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        height: 100%;
-        border-radius: 12px;
-        background-color: #D9D9D9;
-    }
-    .top-container {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        margin: 1rem;
-    }
-    .top-container h3 {
-        font-weight: bold;
-    }
-    .top-container p {
-        margin: .5rem;
-        font-style: italic;
-        font-size: 1.25rem;
-    }
+.container {
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	height: 100%;
+	border-radius: 12px;
+	background-color: #eaeaea;
+}
+.top-container {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	margin: 1rem;
+}
+.top-container h3 {
+	font-weight: bold;
+}
+.top-container p {
+	margin: 0.5rem;
+	font-style: italic;
+	font-size: 1.25rem;
+}
 
-    .title-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        background-color: #C6C4C4;
-        border-radius: 12px 12px 0 0;
-    }
-    .title-container h1 {
-        padding: 1rem;
-        font-size: 2rem;
-    }
+.title-container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	background-color: #d5d4d4;
+	border-radius: 12px 12px 0 0;
+}
+.title-container h1 {
+	padding: 1rem;
+	font-size: 2rem;
+}
 </style>
--- src/views/ExpensesView.vue
+++ src/views/ExpensesView.vue
@@ -1,140 +1,141 @@
 <script setup lang="ts">
-    import { ref } from 'vue'
-    import { computed } from 'vue'
-    import router from '@/router';
-    import NavBar from '../components/NavBar.vue'
-    import ExpensesTable from '../components/ExpensesTable.vue'
-    import FooterComponent from '../components/FooterComponent.vue'
-    import OverviewCard from '../components/OverviewCard.vue'
+import { ref } from 'vue';
+import { computed } from 'vue';
+import router from '@/router';
+import NavBar from '../components/NavBar.vue';
+import ExpensesTable from '../components/ExpensesTable.vue';
+import FooterComponent from '../components/FooterComponent.vue';
+import OverviewCard from '../components/OverviewCard.vue';
 
-    import CreateExpense from '@/components/CreateExpense.vue'
-    import ModalComponent from '@/components/ModalComponent.vue'
-    const modal = ref<InstanceType<typeof ModalComponent>>()
-    const showModal = () => modal.value?.show()
+import CreateExpense from '@/components/CreateExpense.vue';
+import ModalComponent from '@/components/ModalComponent.vue';
+const modal = ref<InstanceType<typeof ModalComponent>>();
+const showModal = () => modal.value?.show();
 
-    import { getUserExpenses } from '@/firebase/database';
-    const expenses = await getUserExpenses();
+import { getUserExpenses } from '@/firebase/database';
+const expenses = await getUserExpenses();
 
-    const latestExpense = computed(() => {
-        return expenses[expenses.length - 1]
-    })
-    const totalAmount = computed(() => {
-        return expenses.reduce((acc, expense) => acc + expense.expenseAmount, 0)
-    })
+const latestExpense = computed(() => {
+	return expenses[expenses.length - 1];
+});
+const totalAmount = computed(() => {
+	return expenses.reduce((acc, expense) => acc + expense.expenseAmount, 0);
+});
 </script>
 
 <template>
-    <NavBar />
-    <main>
-        <ModalComponent ref="modal">
-            <CreateExpense />
-        </ModalComponent>
-        <h2 class="over-view-title">Overview</h2>
-        <div class="overview-cards-container">
-            <OverviewCard
-                class="overview-card"
-                card-title="Total expenses"
-                :card-info="`${totalAmount}kr`"
-            />
-            <OverviewCard
-                class="overview-card"
-                card-title="Total transactions"
-                :card-info="`${expenses.length}st`"
-            />
-            <OverviewCard
-                v-if="expenses.length > 0"
-                class="overview-card"
-                card-title="Average cost"
-                :card-info="`${(totalAmount / expenses.length).toFixed(2)}kr`"
-            />
-            <OverviewCard
-                v-if="latestExpense"
-                class="overview-card"
-                card-title="Latest purchase"
-                :card-info="`${latestExpense.expenseName} - ${latestExpense.expenseAmount}kr`"
-            />
-        </div>
+	<NavBar />
+	<main>
+		<ModalComponent ref="modal">
+			<CreateExpense />
+		</ModalComponent>
+		<h2 class="over-view-title">Overview</h2>
+		<div class="overview-cards-container">
+			<OverviewCard
+				class="overview-card"
+				card-title="Total expenses"
+				:card-info="`${totalAmount}kr`"
+			/>
+			<OverviewCard
+				class="overview-card"
+				card-title="Total transactions"
+				:card-info="`${expenses.length}st`"
+			/>
+			<OverviewCard
+				v-if="expenses.length > 0"
+				class="overview-card"
+				card-title="Average cost"
+				:card-info="`${(totalAmount / expenses.length).toFixed(2)}kr`"
+			/>
+			<OverviewCard
+				v-if="latestExpense"
+				class="overview-card"
+				card-title="Latest purchase"
+				:card-info="`${latestExpense.expenseName} - ${latestExpense.expenseAmount}kr`"
+			/>
+		</div>
 
-        <h3 class="table-heading" v-if="expenses.length > 0">
-            All Expenses
-        </h3>
-        <div class="no-expenses" v-else>
-            <div>
-                <h3 class="table-heading">No expenses yet</h3>
-                <p>Click the button below to add your first expense</p>
-                <button class="add-btn" @click="showModal">Add Your First Expense</button>
-            </div>
-        </div>
-        
-        <ExpensesTable />
-    </main>
-    <FooterComponent />
+		<h3 class="table-heading" v-if="expenses.length > 0">All Expenses</h3>
+		<div class="no-expenses" v-else>
+			<div>
+				<h3 class="table-heading">No expenses yet</h3>
+				<p>Click the button below to add your first expense</p>
+				<button class="add-btn" @click="showModal">
+					Add Your First Expense
+				</button>
+			</div>
+		</div>
+
+		<ExpensesTable />
+	</main>
+	<FooterComponent />
 </template>
 
 <style scoped>
-    main {
-        margin-top: 12vh;
-    }
+main {
+	margin-top: 12vh;
+	margin-bottom: 2rem;
+}
 
-    .over-view-title {
-        text-align: center;
-        font-size: 2.4rem;
-        margin-top: 1rem;
-    }
-    .overview-cards-container {
-        display: flex;
-        justify-content: space-between;
-        margin: auto;
-        margin-top: 1rem;
-        width: 90%;
-        gap: 1.4rem;
-        flex-wrap: wrap;
-    }
+.over-view-title {
+	text-align: center;
+	font-size: 2.4rem;
+	margin-top: 1rem;
+}
+.overview-cards-container {
+	display: flex;
+	justify-content: space-between;
+	margin: auto;
+	margin-top: 1rem;
+	width: 90%;
+	gap: 1.4rem;
+	flex-wrap: wrap;
+}
 
-    .overview-card {
-        flex-grow: 1;
-    }
+.overview-card {
+	flex-grow: 1;
+}
 
-    .table-heading {
-        text-align: center;
-        font-size: 2rem;
-        margin-top: 2rem;
-    }
+.table-heading {
+	text-align: center;
+	font-size: 2rem;
+	margin-top: 2rem;
+}
 
-    .no-expenses {
-        display: flex;
-        justify-content: center;
-        text-align: center;
-        margin-bottom: 2rem;
-    }
+.no-expenses {
+	display: flex;
+	justify-content: center;
+	text-align: center;
+	margin-bottom: 2rem;
+}
 
-    .no-expenses p {
-        margin: 10px;
-    }
+.no-expenses p {
+	margin: 10px;
+}
 
-    .no-expenses > div > button {
-        margin: auto;
-    }
+.no-expenses > div > button {
+	margin: auto;
+}
 
-    .add-btn {
-        border: none;
-        display: block;
-        height: 60px;
-        font-size: 1rem;
-        text-align: center;
-        font-weight: 600;
-        padding: 0.8rem 2.5rem;
-        border: none;
-        border-radius: 30px;
-        background-color: var(--secondary-color);
-        color: white;
-        text-decoration: none;
-        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-        transition: background-color 0.3s ease;
-    }
+.add-btn {
+	border: none;
+	display: block;
+	height: 60px;
+	font-size: 1rem;
+	text-align: center;
+	font-weight: 600;
+	padding: 0.8rem 2.5rem;
+	border: none;
+	border-radius: 30px;
+	background-color: var(--secondary-color);
+	color: white;
+	text-decoration: none;
+	box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+	transition: background-color 0.3s ease;
+}
 
-    .add-btn:hover {
-        background: #257c29;
-        cursor: pointer;
-    }
+.add-btn:hover {
+	background: #257c29;
+	cursor: pointer;
+}
 </style>
