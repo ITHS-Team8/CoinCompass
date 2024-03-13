930636fc2f95c1720668b756b356d2d9cd8e4273
hydrarguru <henrik@engqvist.org>
2024-03-13 20:06:21 +0100

    refactor: removed edit button.

--- src/components/ExpensesTable.vue
+++ src/components/ExpensesTable.vue
@@ -119,17 +119,6 @@ const filteredItems = computed(() => {
 				</div>
 			</div>
 			<div class="btn-container">
-				<button class="edit-btn" type="button" @click="showEditModal(expense.expenseId)">
-					<svg
-						style="height: 24px"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 512 512"
-					>
-						<path
-							d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"
-						/>
-					</svg>
-				</button>
 				<button
 					class="delete-btn"
 					type="button"
@@ -161,30 +150,20 @@ const filteredItems = computed(() => {
 				<th>Name</th>
 				<th class="sum-th">Sum</th>
 				<th>Category</th>
-				<th>Buttons</th>
+				<th>Actions</th>
 			</tr>
 		</thead>
 		<tbody>
-			<tr v-for="expense in filteredItems">
+			<tr v-for="expense in filteredItems" :id="expense.expenseId">
 				<td class="name-td">{{ expense.expenseName }}</td>
 				<td class="sum-td">{{ expense.expenseAmount }}kr</td>
 				<td>{{ expense.expenseCategory }}</td>
 				<td>
 					<div class="mobile-btn-container">
-						<button class="edit-btn" type="button" @click="">
-							<svg
-								style="height: 20px"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 512 512"
-							>
-								<path
-									d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"
-								/>
-							</svg></button
-						><button
+						<button
 							class="delete-btn"
 							type="button"
-							@click="deleteUserExpense(expense.expenseId)"
+							@click="deleteUserExpenseAndElement(expense.expenseId)"
 						>
 							<svg
 								style="height: 20px"
@@ -271,7 +250,7 @@ h2 {
 	width: 80%;
 	display: inline-flex;
 	justify-content: space-between;
-	margin-left: 4.3rem;
+	margin-left: 6rem;
 }
 .titles p {
 	font-size: 1.1rem;
@@ -292,7 +271,7 @@ h2 {
 	display: inline-flex;
 	justify-content: space-between;
 	align-items: center;
-	width: 90%;
+	width: 100%;
 	background-color: #d5d4d4;
 	border-radius: 20px;
 	padding: 20px 2px;
